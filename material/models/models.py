7fff5b8c016f2065ec354244c04f0f6d1b536f0f
Azka Chaniago <azka24chaniago@gmail.com>
2024-07-13 04:54:53 +0700

    feat: create models for materials and buy price  constrains

--- material/models/models.py
+++ material/models/models.py
@@ -1,18 +1,23 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 
-# class material(models.Model):
-#     _name = 'material.material'
-#     _description = 'material.material'
+class Material(models.Model):
+    _name = 'material.material'
+    _description = 'Material'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char()
+    code = fields.Char()
+    type = fields.Selection([('fabric', 'Fabric'),
+                             ('jeans', 'Jeans'),
+                             ('cotton', 'Cotton')], default='fabric')
+    buy_price = fields.Integer()
+    supplier_id = fields.Many2one('res.partner')
+
+    @api.constrains('buy_price')
+    def _validate_buy_price(self):
+        for record in self:
+            if record.buy_price < 100:
+                raise ValidationError("Buy Price value should not less than 100!")
