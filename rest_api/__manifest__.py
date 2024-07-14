1668ccf20b38673378894c6d31c2db2701660818
Azka Chaniago <azka24chaniago@gmail.com>
2024-07-14 14:05:15 +0700

    docs: update manifest

--- rest_api/__manifest__.py
+++ rest_api/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "rest_api",
+    'name': "REST API",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -10,8 +10,8 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Muhammad Fadhil Azka",
+    'website': "https://www.linkedin.com/in/muhammad-fadhil-azka/",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
