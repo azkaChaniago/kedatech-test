04272f3f5460349f1d74c8959176b2d3acd79517
Azka Chaniago <azka24chaniago@gmail.com>
2024-07-14 10:16:10 +0700

    test: fixing test case

--- material/models/models.py
+++ material/models/models.py
@@ -8,13 +8,13 @@ class Material(models.Model):
     _name = 'material.material'
     _description = 'Material'
 
-    name = fields.Char(default=lambda self: self.env['ir.sequence'].next_by_code('material.material'))
-    code = fields.Char()
+    name = fields.Char(default=lambda self: self.env['ir.sequence'].next_by_code('material.material'), required=1)
+    code = fields.Char(required=1)
     type = fields.Selection([('fabric', 'Fabric'),
                              ('jeans', 'Jeans'),
-                             ('cotton', 'Cotton')], default='fabric')
-    buy_price = fields.Integer()
-    supplier_id = fields.Many2one('res.partner')
+                             ('cotton', 'Cotton')], default='fabric', required=1)
+    buy_price = fields.Integer(required=1)
+    supplier_id = fields.Many2one('res.partner', required=1)
 
     @api.constrains('buy_price')
     def _validate_buy_price(self):
--- material/tests/test_models.py
+++ material/tests/test_models.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from odoo.tests.common import SavepointCase
-from odoo.exceptions import ValidationError
+from odoo.exceptions import ValidationError, UserError
 from odoo.tests import tagged
+
+from psycopg2.errors import NotNullViolation
 
 # The CI will run these tests after all the modules are installed,
 # not right after installing the one defining it.
@@ -18,22 +20,28 @@ class MaterialTestCase(SavepointCase):
         # of in a setUp or in each test case, we reduce the testing time and
         # the duplication of code.
         cls.properties = cls.env['material.material'].create([
-            {'code': 'A001', 'buy_price': 200, 'type': 'jeans'},
-            {'code': 'A002', 'buy_price': 400, 'type': 'fabric'},
-            {'code': 'A003', 'buy_price': 270, 'type': 'cotton'},
+            {'code': 'A001', 'buy_price': 200, 'type': 'jeans', 'supplier_id': 4},
+            {'code': 'A002', 'buy_price': 400, 'type': 'fabric', 'supplier_id': 2},
+            {'code': 'A003', 'buy_price': 270, 'type': 'cotton', 'supplier_id': 3},
         ])
 
 
     def test_create(self):
         """Test that everything behaves like it should when creating a material."""
         self.assertRecordValues(self.properties, [
-            {'code': 'A001', 'buy_price': 200, 'type': 'jeans'},
-            {'code': 'A002', 'buy_price': 400, 'type': 'fabric'},
-            {'code': 'A003', 'buy_price': 270, 'type': 'cotton'},
+            {'code': 'A001', 'buy_price': 200, 'type': 'jeans', 'supplier_id': 4},
+            {'code': 'A002', 'buy_price': 400, 'type': 'fabric', 'supplier_id': 2},
+            {'code': 'A003', 'buy_price': 270, 'type': 'cotton', 'supplier_id': 3},
         ])
 
     def test_constrains_buy_price(self):
-        with self.assertRaises(ValidationError):
+        with self.assertRaises(ValidationError), self.cr.savepoint():
             self.properties.create([
-                {'code': 'A003', 'buy_price': 70, 'type': 'cotton'},
+                {'code': 'A003', 'buy_price': 70, 'type': 'cotton', 'supplier_id': 5},
+            ])
+    
+    def test_empty_code_field(self):
+        with self.assertRaises(NotNullViolation), self.cr.savepoint():
+            self.properties.create([
+                {'buy_price': 700, 'type': 'cotton', 'supplier_id': 10},
             ])
